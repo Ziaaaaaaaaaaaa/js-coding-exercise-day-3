20ff26afd2b97150b4e0e41a4d5877c8e01d4a66
Ziaaaaaaaaaaaa <ziakhan29042003gmail.com>
2023-05-19 08:44:39 +0200

    commit

---
+++ script/code.js
@@ -0,0 +1,18 @@
+let firstName = "Zia";
+console.log(typeof firstName);
+
+let number = 29;
+console.log(typeof number);
+
+let undefined = ;
+console.log(typeof undefined);
+
+//boolean
+let selected = true ;
+console.log(typeof selected);
+//null
+let lastName = null
+console.log(typeof lastName);
+//number type
+let numb = 30.5 ;
+console.log(typeof numb);
